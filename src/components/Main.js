4e6a7a37b1fd1d42f7922ae3af148d2fbd318ce1
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-05-07 11:02:36 -0300

    styling the form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import './Main.css'
+import './Main.css';
+
+// Form
+import { FaPlus } from 'react-icons/fa';
 
 
 export default class Main extends Component {
@@ -17,10 +20,12 @@ export default class Main extends Component {
     const { novaTarefa } = this.state;
     return (
       <div className='main'>
-        <h1>{novaTarefa}</h1>
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <h1>Lista de tarefas</h1>
+        <form action="#" className='form'>
+          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     );
