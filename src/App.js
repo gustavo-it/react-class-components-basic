2158c7ff3778c97c9c593f00d370a2a5cfac6924
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-05-07 10:50:22 -0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
+import Main from './components/Main';
 
-// componente sem estado ==> função normal que retorna jsx
 
 export default function App() {
-  return <h1>Hello world!</h1>;
+  return <Main />;
 }
-
-// export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Main.css'
+
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      novaTarefa: event.target.value,
+    });
+  }
+
+  render() {
+    const { novaTarefa } = this.state;
+    return (
+      <div className='main'>
+        <h1>{novaTarefa}</h1>
+        <form action="#">
+          <input onChange={this.handleChange} type="text" />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
+  }
+}
