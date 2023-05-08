a9ed537bf4ae8cb782ca7256d49dd1558087425f
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-05-08 19:27:31 -0300

    Stylizing the tasks

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,10 +4,16 @@ import './Main.css';
 // Form
 import { FaPlus } from 'react-icons/fa';
 
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 
 export default class Main extends Component {
   state = {
-    novaTarefa: '',
+    tarefas: [
+      'Fazer café',
+      'Ir ao supermercado',
+    ],
   };
 
   handleChange = (event) => {
@@ -17,7 +23,7 @@ export default class Main extends Component {
   }
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
     return (
       <div className='main'>
         <h1>Lista de tarefas</h1>
@@ -27,6 +33,19 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+        <ul className='tarefas'>
+          {tarefas.map(tarefa => (
+            // Cada elemento dentro de um laço, precisa ter uma propriedade key
+            // com um valor único em cada iteração.
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+              <FaEdit className='edit' />
+              <FaWindowClose className='delete' />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
