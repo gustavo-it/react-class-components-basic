15ec4c6ec78ff9af00adb91641efcff96819f8d5
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-05-14 14:38:10 -0300

    Editing tasks

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,23 +10,34 @@ import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
 export default class Main extends Component {
   state = {
-    tarefas: [
-    ],
+    // novaTarefa: '',
+    tarefas: [],
+    index: -1,
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
-    novaTarefa = novaTarefa.trim(); // elimina espaços do inicio e fim
+    novaTarefa = novaTarefa.trim();
 
     if(tarefas.indexOf(novaTarefa) !== -1) return;
 
     const novasTarefas = [...tarefas];
 
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-    })
+    if(index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
   }
 
   handleChange = (event) => {
@@ -36,7 +47,12 @@ export default class Main extends Component {
   }
 
   handleEdit = (event, index) => {
+    const { tarefas } = this.state;
 
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    })
   }
 
   handleDelete = (event, index) => {
@@ -45,8 +61,8 @@ export default class Main extends Component {
     novasTarefas.splice(index, 1);
 
     this.setState({
-      tarefas: [...novasTarefas,]
-    })
+      tarefas: [...novasTarefas,],
+    });
   }
 
   render() {
